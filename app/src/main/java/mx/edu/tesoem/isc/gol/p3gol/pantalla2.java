55e0a1145217eb6b1c00fce4a1a421fe2ef4e58a
Gustavo Ortega Lopez <ortegalopezgustavo1998@gmail.com>
2019-10-08 21:09:20 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/gol/p3gol/pantalla2.java
@@ -0,0 +1,13 @@
+package mx.edu.tesoem.isc.gol.p3gol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pantalla2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla2);
+    }
+}
